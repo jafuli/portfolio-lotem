357dfb09697801b749ee078b85d299a81648d895
itamarbadash <itamar@opster.com>
2025-05-17 11:37:34 +0300

    Not found page added

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,6 +1,27 @@
+import {css} from "@emotion/react";
+import Button from "../../components/Button/Button.tsx";
+
 const NotFound = () => {
+
   return (
-    <>Page Not Found</>
+    <>
+      <div css={css`
+        margin: auto;
+        display: flex;
+        gap: 2rem;
+        flex-direction: column;
+        width: 75%;
+      `}>
+        <div css={css`
+          font-size: 3.75rem;
+          letter-spacing: -0.08em;
+          white-space: nowrap;
+          text-align: center;
+        `}>I HAVE NO IDEA HOW YOU <br/>GOT HERE
+        </div>
+        <Button route={'/works'} buttonName={'Go to a better place'}/>
+      </div>
+    </>
   );
 }
 
